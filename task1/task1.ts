b1ab536701667681474e493cefcf60a2e15b481b
Kulizon <kulixon261@gmail.com>
2023-04-14 15:49:01 +0200

    change task1 tree structure

--- task1/task1.ts
+++ task1/task1.ts
@@ -1,96 +1,117 @@
+// assuming that the structure has two children at most, for better scalability you could swap 'left' and 'right' with 'children' array
 interface NodeInterface {
   val: number;
-  children: NodeInterface[]; // todo -> add length of array
+  left: NodeInterface | null;
+  right: NodeInterface | null;
 }
 
 const root: NodeInterface = {
   val: 5,
-  children: [
-    {
-      val: 3,
-      children: [
-        { val: 2, children: [] },
-        { val: 5, children: [] },
-      ],
+  left: {
+    val: 3,
+    left: {
+      val: 2,
+      left: null,
+      right: null,
     },
-    {
-      val: 7,
-      children: [
-        { val: 1, children: [] },
-        {
-          val: 0,
-          children: [
-            { val: 2, children: [] },
-            { val: 8, children: [{ val: 5, children: [] }] },
-          ],
+    right: { val: 5, left: null, right: null },
+  },
+  right: {
+    val: 7,
+    left: {
+      val: 1,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 0,
+      left: { val: 2, left: null, right: null },
+      right: {
+        val: 8,
+        left: null,
+        right: {
+          val: 5,
+          left: null,
+          right: null,
         },
-      ],
+      },
     },
-  ],
+  },
 };
 
 const fake: NodeInterface = {
   val: 5,
-  children: [
-    {
-      val: 3,
-      children: [
-        { val: 2, children: [] },
-        { val: 5, children: [] },
-      ],
+  left: {
+    val: 3,
+    left: {
+      val: 2,
+      left: null,
+      right: null,
     },
-    {
-      val: 7,
-      children: [
-        { val: 1, children: [] },
-        {
-          val: 0,
-          children: [
-            { val: 2, children: [] },
-            { val: 8, children: [{ val: 5, children: [] }] },
-          ],
+    right: { val: 5, left: null, right: null },
+  },
+  right: {
+    val: 7,
+    left: {
+      val: 1,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 0,
+      left: { val: 2, left: null, right: null },
+      right: {
+        val: 8,
+        left: null,
+        right: {
+          val: 5,
+          left: null,
+          right: null,
         },
-      ],
+      },
     },
-  ],
+  },
 };
 
-const countLeaves = (node: NodeInterface): number => {
-  if (node.children.length === 0) return 1;
+const countLeafs = (node: NodeInterface | null): number => {
+  if (!node) return 0;
+  if (!node.left && !node.right) return 1;
   let sum = 0;
-  for (let i = 0; i < node.children.length; i++) {
-    sum += countLeaves(node.children[i]);
-  }
+
+  sum += countLeafs(node?.left);
+  sum += countLeafs(node?.right);
+
   return sum;
 };
 
 const calculateDepth = (initialNode: NodeInterface): number => {
   let max = 0;
   const traverse = (node: NodeInterface, cur: number) => {
-    if (node.children.length === 0 && cur > max) max = cur;
+    if (!node.left && !node.right && cur > max) max = cur;
 
-    for (let i = 0; i < node.children.length; i++) {
-      traverse(node.children[i], cur + 1);
-    }
+    if (node.left) traverse(node.left, cur + 1);
+    if (node.right) traverse(node.right, cur + 1);
   };
-
   traverse(initialNode, 0);
   return max;
 };
 
 // assuming that the order of children in children array matters
-const areIdentical = (node1: NodeInterface, node2: NodeInterface): boolean => {
-  let areChildrenIdentical = node1.children.length === node2.children.length;
+const areIdentical = (
+  node1: NodeInterface | null,
+  node2: NodeInterface | null
+): boolean => {
+  if (!(node1 && node2)) return node1 === node2;
+  let isLeftSideIdentical = areIdentical(node1.left, node2.left);
+  let isRightSideIdentical = areIdentical(node1.right, node2.right);
 
-  let i = 0;
-  while (areChildrenIdentical && i < node1.children.length) {
-    areChildrenIdentical = areIdentical(node1.children[i], node2.children[i]);
-    i++;
-  }
-
-  return node1.val === node2.val && areChildrenIdentical;
+  return !!(
+    node1?.val === node2?.val &&
+    isLeftSideIdentical &&
+    isRightSideIdentical
+  );
 };
 
-// console.log(countLeaves(root));
-// console.log(calculateDepth(root));
+console.log(countLeafs(root));
+console.log(calculateDepth(root));
 console.log(areIdentical(root, fake));
