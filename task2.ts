d14935a9ca16cd81315ca37fd63e079749417072
Kulizon <kulixon261@gmail.com>
2023-04-14 13:26:27 +0200

    task 2: add first subtask, update fetchResponse return types

--- task2.ts
+++ task2.ts
@@ -1,23 +1,65 @@
 import fetch from "node-fetch";
 
 const USERS_URL = "https://fakestoreapi.com/users";
+const CARTS_URL =
+  "https://fakestoreapi.com/carts/?startdate=2000-01-01&enddate=2023-04-07";
+const PRODUCTS_URL = "https://fakestoreapi.com/products";
 
-interface FetchResponseInterface {
+interface FetchResponseInterface<DataType> {
   status: 0 | -1;
-  data: unknown;
+  data: DataType | null;
 }
 
-const fetchData = async (fetchUrl: string): Promise<FetchResponseInterface> => {
+interface UsersDataInterface {}
+interface CartsDataInterface {}
+interface ProductsDataInterface {
+  category: string;
+  price: number;
+}
+
+interface ProductCategoryInterface {
+  [key: string]: number;
+}
+
+const fetchData = async <DataType>(
+  fetchUrl: string
+): Promise<FetchResponseInterface<DataType>> => {
   try {
     const response = await fetch(fetchUrl);
     const data = await response.json();
     return { status: 0, data: data };
   } catch (e) {
     console.log(e);
-    return { status: -1, data: e };
+    return { status: -1, data: null };
   }
 };
 
-const data = await fetchData(USERS_URL);
-console.log(data);
+const { status: userResponseStatus, data: userData } = await fetchData<
+  UsersDataInterface[]
+>(USERS_URL);
+const { status: cartsResponseStatus, data: cartsData } = await fetchData<
+  CartsDataInterface[]
+>(CARTS_URL);
+const { status: productsResponseStatus, data: productsData } = await fetchData<
+  ProductsDataInterface[]
+>(PRODUCTS_URL);
 
+const retrieveProductCategories = (
+  data: ProductsDataInterface[] | null,
+  status: 0 | -1
+): ProductCategoryInterface => {
+  if (status === -1 || !data) {
+    console.log("Error retriving products data!");
+    return {};
+  }
+
+  const output: ProductCategoryInterface = {};
+  data.map((prod) => {
+    if (!output[prod.category]) output[prod.category] = 0;
+    output[prod.category] += prod.price;
+  });
+
+  return output;
+};
+
+console.log(retrieveProductCategories(productsData, productsResponseStatus));
